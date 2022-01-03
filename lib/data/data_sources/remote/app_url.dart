dbf111eb5a725ec5875cfba273fbd1329b7d4b93
Kin <>
2022-01-03 23:44:52 +0700

    aa

---
+++ lib/data/data_sources/remote/app_url.dart
@@ -0,0 +1,3 @@
+class AppUrl{
+  static const String task = 'https://raw.githubusercontent.com/kingcut/flutter_boilerplate/main/test/tasks.json';
+}
--- lib/data/data_sources/remote/rest_client.dart
+++ lib/data/data_sources/remote/rest_client.dart
@@ -1,23 +1,32 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_boilerplate/data/data_sources/common/response_data.dart';
 import 'package:flutter_boilerplate/data/data_sources/common/response_list_data.dart';
 import 'package:flutter_boilerplate/data/data_sources/remote/request.dart';
 import 'package:flutter_boilerplate/data/models/task.dart';
 import 'package:retrofit/retrofit.dart';
+// import 'package:json_annotation/json_annotation.dart';
 
 // part 'rest_client.g.dart';
 
 // @RestApi(
 //     baseUrl:
+//         "hti(
+//     baseUrl:
 //         "https://raw.githubusercontent.com/kingcut/flutter_boilerplate/main/test/")
 class RestClient {
   final Request request;
-  RestClient(this.request)
+  RestClient(this.request);
+  // factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
+
 
   // @GET("/tasks.json")
   // Future<ResponseListData<Task>> getTasks(Function(Map<String, dynamic>) factoryFunction);
-  Future<ResponseListData<Task>> getTasks() async {
-    request.get(url)
-  }
 
+
+  Future<ResponseListData<Task>?>? getTasksA() async {
+    final response = await request.get(Uri.http('https://raw.githubusercontent.com/kingcut/flutter_boilerplate/main/test/', '/tasks.json'));
+    return ResponseListData<Task>.fromJson(json.decode(response.body),(item) => Task.fromJson(item));
+  }
 }

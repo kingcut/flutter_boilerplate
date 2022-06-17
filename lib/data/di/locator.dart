002a387fff0ea1853eec87fc68c4b24f2e43e325
Kinnd <>
2022-06-17 17:21:16 +0700

    shared preference

--- lib/data/di/locator.dart
+++ lib/data/di/locator.dart
@@ -5,16 +5,19 @@ import 'package:flutter_boilerplate/data/data_sources/remote/rest_client.dart';
 import 'package:flutter_boilerplate/data/flavor/prod_config.dart';
 import 'package:flutter_boilerplate/data/flavor/staging_config.dart';
 import 'package:flutter_boilerplate/data/repositories/task_repo_impl.dart';
+import 'package:flutter_boilerplate/data/shared_pref/shared_preference_helper.dart';
 import 'package:flutter_boilerplate/domain/repositories/task_repo.dart';
 import 'package:flutter_boilerplate/domain/flavor/flavor_config.dart';
 import 'package:flutter_boilerplate/data/flavor/dev_config.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final locator = GetIt.instance..allowReassignment = true;
 
 Future setupLocator(String? flavor) async {
   HttpOverrides.global = MyHttpOverrides();
   _registerClient();
+  _registerSharedPref();
   _registerRepository();
   _registerEnvironment(flavor);
 }
@@ -32,6 +35,14 @@ FlavorConfig _getConfig(String? flavor) {
     default:
       return DevConfig();
   }
+}
+
+Future _registerSharedPref() async {
+  locator.registerSingletonAsync<SharedPreferences>(
+      () => SharedPreferences.getInstance());
+
+  locator.registerSingleton(
+      SharedPreferenceHelper(await locator.getAsync<SharedPreferences>()));
 }
 
 Future _registerClient() async {
